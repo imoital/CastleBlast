1a1cb13f9a9f41bd816d2603bfa93c8bdf8f30b8
Nuno Costa <nuno_costa@live.com>
2011-12-03 04:42:14 +0000

    CannonCamera is rotating properly. Still not positioned where the cannon is though

--- CanonCamera.cpp
+++ CanonCamera.cpp
@@ -7,7 +7,6 @@
 //
 
 #include "CanonCamera.h"
-
 namespace CastleBlast {
 	
 	CanonCamera::CanonCamera(Cannon *cannon) : Entity("CANON_CAMERA") 
@@ -22,9 +21,10 @@ namespace CastleBlast {
 		cg::tWindowInfo win = cg::Manager::instance()->getApp()->getWindowInfo();
 		_winWidth = win.width;
 		_winHeight = win.height;
-		_orientation.setRotationDeg(0,cg::Vector3d::ny);
-		_eye.set(0,0,0);
-		_center.set(0,0,0);
+		_anglex = 0;
+		_angley = 0;
+		_eye.set(0,10,0);
+		_center.set(10,10,0);
 		_eyeInc.set(0,0,0);
 		_centerInc.set(0,5,0);
 		_up.set(0,1,0);
@@ -32,28 +32,21 @@ namespace CastleBlast {
 		_right.set(0,0,1);
 		_isRoll = false;
 		_scale = 5.0f;
-		_cameraSpeed = cg::Properties::instance()->getInt("CAMERA_SPEED");
+		_cannonMouseSpeed = cg::Properties::instance()->getDouble("CANNON_MOUSE_SPEED");
 		
-		/* Initialize camera position */
-		_q.setRotationDeg(90,_up);
-		_front = apply(_q,_front);
-		_right = apply(_q,_right);
-		_q.setRotationDeg(50, _right);
-		_front = apply(_q,_front);
 	}
 	void CanonCamera::draw() {
 		_cannonPos = _cannon->getPosition(); 
-		_eye = _front * _scale;
 		glMatrixMode(GL_PROJECTION);
 		glLoadIdentity();
 		gluPerspective(65, _winWidth/(double)_winHeight, 1, 500);
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
-		gluLookAt(_eye[0]+_eyeInc[0] + _cannonPos[0]+3, _eye[1]+_eyeInc[1] + _cannonPos[1], _eye[2]+_eyeInc[2]+_cannonPos[2],
-			_center[0]+_centerInc[0]+_cannonPos[0]+3, _center[1]+_centerInc[1]+ _cannonPos[1], _center[2]+_centerInc[2]+_cannonPos[2],
+		gluLookAt(_eye[0], _eye[1], _eye[2],
+			_center[0], _center[1], _center[2],
 			_up[0], _up[1], _up[2]);
 	//	std::cout << _eye[0]+_eyeInc[0]<< " " << _eye[1]+_eyeInc[1]<< " " << _eye[2]+_eyeInc[2] << std::endl;
-		std::cout << _cannonPos[0] << " " << _cannonPos[1] << " " << _cannonPos[2] << std::endl;
+	//	std::cout << _cannonPos[0] << " " << _cannonPos[1] << " " << _cannonPos[2] << std::endl;
 	}
 
 	void CanonCamera::update(unsigned long elapsed_millis)
@@ -68,31 +61,23 @@ namespace CastleBlast {
 
 	void CanonCamera::onMouse(int button, int state, int x, int y)
 	{
-		_isRoll = (button == GLUT_RIGHT_BUTTON);
 		_lastMousePosition.set(x,y);
-		if (state == GLUT_UP) {
-			if (button == GLUT_WHEEL_UP)
-				_scale -= 0.5f;
-
-			else if (button == GLUT_WHEEL_DOWN)
-				_scale += 0.5f;
-		}
 	}
 
 	void CanonCamera::onMouseMotion(int x, int y)
-	{
-		double anglex = (_lastMousePosition[0] - x) / (double)5;
-			_q.setRotationDeg(anglex,_up);
-			_front = apply(_q,_front);
-			_right = apply(_q,_right);
-		double angley = (y - _lastMousePosition[1]) / (double)5;
-			_q.setRotationDeg(angley, _right);
-			_front = apply(_q,_front);
+	{	
+		_anglex += _cannonMouseSpeed*(_lastMousePosition[0] - x)*PI/180;
+		_angley += _cannonMouseSpeed*(_lastMousePosition[1] - y)*PI/180;
+		std::cout << sin(_angley) << std::endl;
+		_center[0] = 10*cos(_anglex);
+		_center[2] = 10*sin(_anglex);
+		_center[1] = -30*sin(_angley);
 		_lastMousePosition.set(x,y);
 	}
 
 	void CanonCamera::onMousePassiveMotion(int x, int y)
 	{
+
 	}
 	void CanonCamera::onReshape(int width, int height) 
 	{
